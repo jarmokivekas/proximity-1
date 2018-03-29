d2627c46ab3f39eeb497a60768550b73eb2a72b2
Jarmo Kivekäs <jarmo.kivekas@gmail.com>
2018-03-29 19:29:57 +0300

    init

---
+++ lfsr.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stdint.h>
+
+
+class LDCP_encoder {
+private:
+    uint8_t lfsr_generator_state;
+
+public:
+    LDCP_encoder (arguments);
+    virtual ~LDCP_encoder ();
+
+};
+LDCP_encoder::LDCP_encoder(){
+    this->lfsr_generator_state = 0xff;
+}
+
+LDCP_encoder::next_random_bit(){
+    uint8_t newbit;
+    uint8_t outbit;
+    uint8_t x = this->x;
+    for (size_t i = 0; i < 40; i++) {
+        outbit = x & 0x01;
+        newbit = (
+            x ^
+            (x >> 1) ^
+            (x >> 2) ^
+            (x >> 3) ^
+            (x >> 4) ^
+            (x >> 6)
+            ) & 1;
+        this->x = (x >> 1) | (newbit << 7);
+    }
+    return this->x
+}
+
+    return 0;
+}
